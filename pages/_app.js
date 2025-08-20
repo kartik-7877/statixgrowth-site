dea4d0dd65caff23edff179564b55f0a0ba8489d
kartik bhaskar <support@statixgrowth.info>
2025-08-20 13:32:27 +0530

    fix: switched to next/font Montserrat for consistent font loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,26 @@
-import '../styles/globals.css';
-import Navbar from '../components/Navbar';
+import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import { Montserrat } from 'next/font/google'
+
+// Import Montserrat via next/font
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  display: 'swap',
+})
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <main className={montserrat.className}>
       <Navbar />
       <Component {...pageProps} />
-    </>
-  );
+    </main>
+  )
 }
 
-export default MyApp;
+export default MyApp
+
+
+
 
 
