5f9bce6f54ba8477243050179b6b4f58ea4dc05d
kartik bhaskar <support@statixgrowth.info>
2025-08-20 17:09:22 +0530

    Fix: remove extra mobile spacing in HeroSection

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -68,7 +68,7 @@ export default function HeroSection() {
   }, []);
 
   return (
-    <section className="relative w-full min-h-[78vh] md:min-h-0 md:h-screen flex items-center justify-center overflow-hidden py-4 sm:py-6 md:py-0">
+    <section className="relative w-full md:h-screen flex items-center justify-center overflow-hidden py-4 sm:py-6 md:py-0">
       {/* Background Canvas */}
       <canvas ref={canvasRef} className="absolute inset-0 z-0" />
 
@@ -119,6 +119,7 @@ export default function HeroSection() {
     </section>
   );
 }
+
 
 
 
