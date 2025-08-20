65482db4cf4bb47855385756f657790e4a822413
kartik bhaskar <support@statixgrowth.info>
2025-08-20 14:26:20 +0530

    feat: add Playfair Display font for headings and set up Montserrat as default body font

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,26 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
-import { Montserrat } from 'next/font/google'
+import { Montserrat, Playfair_Display } from 'next/font/google'
 
-// Import Montserrat via next/font
+// Montserrat → for body (sans)
 const montserrat = Montserrat({
   subsets: ['latin'],
-  weight: ['400', '600', '700'],
+  weight: ['400','500','600','700'],
   display: 'swap',
+  variable: '--font-sans',
+})
+
+// Playfair Display → for headings (serif, elegant)
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  weight: ['600','700','800'],
+  display: 'swap',
+  variable: '--font-display',
 })
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={montserrat.className}>
+    <main className={`${montserrat.variable} ${playfair.variable} font-sans`}>
       <Navbar />
       <Component {...pageProps} />
     </main>
@@ -19,6 +28,7 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
 
 
 
